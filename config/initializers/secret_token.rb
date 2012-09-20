31e8a657f3576f7cc3a3bead625f304447566da8
mostlyclicks <carlos@mostlyclicks.com>
2012-09-20 16:11:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aea2::Application.config.secret_token = 'efb306354d56b89b47ee360ee71b5439d155d82a1e55e93592d6385dadf0804a0a29a97e864ec47c93ed113e41695bf277b2b418492f337590231cdc1afd4c9c'
