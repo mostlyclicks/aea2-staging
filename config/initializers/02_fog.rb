a8ca21546dbe359fbeb447636f9d2841982c9a87
mostlyclicks <carlos@mostlyclicks.com>
2012-10-08 09:01:04 -0500

    fog code formatting

--- config/initializers/02_fog.rb
+++ config/initializers/02_fog.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
 	config.root = Rails.root.join('tmp')
 	config.cache_dir = 'uploads'
-	
+
 	config.fog_credentials = {
 		:provider					=> 'AWS',
 		:aws_access_key_id			=> ENV['S3_KEY_ACCESS'],
@@ -12,6 +12,4 @@ CarrierWave.configure do |config|
 	config.fog_public = true
 	config.fog_attributes = {}
 	#config.asset_host = {}
-
-
 end
