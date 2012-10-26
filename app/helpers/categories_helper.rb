93040ba4b99ace9af06158fa67f91b29650c96bb
mostlyclicks <carlos@mostlyclicks.com>
2012-10-26 10:19:27 -0500

    add styles

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,17 @@
 module CategoriesHelper
+	def assets_present(category)
+		if category.assets.present?
+        	"span4"
+        else
+        	"span10"
+        end
+	end
 end
+
+
+#<dl>
+#            <dt><%= link_to ca.name, ca %></dt>
+#                <% if ca.aea_file_url.present? %>
+#                <dd><%= link_to 'Download file', ca.aea_file_url %></dd>
+#                <% end %>
+#            </dl>
