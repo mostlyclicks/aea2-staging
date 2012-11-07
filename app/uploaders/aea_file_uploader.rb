33bf6a11fca09d42013e6c0cd033309f315c9dc4
mostlyclicks <carlos@mostlyclicks.com>
2012-11-07 14:14:04 -0600

    aea default image

--- app/uploaders/aea_file_uploader.rb
+++ app/uploaders/aea_file_uploader.rb
@@ -27,13 +27,15 @@ class AeaFileUploader < CarrierWave::Uploader::Base
     "#{Rails.root}/tmp/uploads"
   end
 
+
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+   def default_url
+     # For Rails 3.1+ asset pipeline compatibility:
+      asset_path("fallback/" + [version_name, "top-logo.png"].compact.join('_'))
+  
+     
+   end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
