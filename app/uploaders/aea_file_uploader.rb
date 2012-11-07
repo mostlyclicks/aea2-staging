2c448a2d1acb12f1b0893f8809db9952697ce5ec
mostlyclicks <carlos@mostlyclicks.com>
2012-11-07 14:20:46 -0600

    took out default image

--- app/uploaders/aea_file_uploader.rb
+++ app/uploaders/aea_file_uploader.rb
@@ -30,12 +30,9 @@ class AeaFileUploader < CarrierWave::Uploader::Base
 
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-   def default_url
-     # For Rails 3.1+ asset pipeline compatibility:
-      asset_path("fallback/" + [version_name, "top-logo.png"].compact.join('_'))
-  
-     
-   end
+    #def default_url
+    #"/images/fallback/" + [version_name, "default.png"].compact.join('_')
+    #end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
