c9e83027c2115cff80fc3ff5e58a4d0aff68fe42
mostlyclicks <carlos@mostlyclicks.com>
2013-01-10 20:54:17 -0600

    upgrade to 3.2.11

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,5 +35,5 @@ Aea2::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.assets.precompile += %w( .svg .eot .woff .ttf )
+  #config.assets.precompile += %w( .svg .eot .woff .ttf )
 end
