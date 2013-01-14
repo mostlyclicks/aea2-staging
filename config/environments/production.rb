0bad546e71f3f29dd5f5e8ec13d2af1254df67ec
mostlyclicks <carlos@mostlyclicks.com>
2013-01-14 16:51:27 -0600

    config.assets.compile = false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Aea2::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
   #config.assets.precompile += %w( .svg .eot .woff .ttf )
 
   # Generate digests for assets URLs
