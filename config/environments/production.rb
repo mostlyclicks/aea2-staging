750b9485b9a6063593f49c19c894a89859e9b648
mostlyclicks <carlos@mostlyclicks.com>
2013-01-15 10:37:15 -0600

    production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,9 @@ Aea2::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Heroku REQUIRES this to be false
+  config.assets.initialize_on_precompile = false
+
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
@@ -15,8 +18,8 @@ Aea2::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
-  #config.assets.precompile += %w( .svg .eot .woff .ttf )
+  config.assets.compile = true
+  config.assets.precompile += %w( .png .svg .eot .woff .ttf )
 
   # Generate digests for assets URLs
   config.assets.digest = true
