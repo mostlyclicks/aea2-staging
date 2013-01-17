233659f9e7bc5c559fd3da0894994ca4cbd93dc8
mostlyclicks <carlos@mostlyclicks.com>
2013-01-17 10:18:24 -0600

    heroku compile fix

--- config/application.rb
+++ config/application.rb
@@ -60,6 +60,8 @@ module Aea2
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    config.assets.initialize_on_precompile = false
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
